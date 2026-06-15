5dedd657c8514e1c27ad398fc4a56e7262a70fe2
Jules Stremersch <j.stremersch@gmail.com>
2026-06-15 12:40:12 +0200

    added asusctl power toggle

--- toggle_laptop_screen.sh
+++ toggle_laptop_screen.sh
@@ -3,8 +3,8 @@
 status=$(hyprctl monitors | grep eDP-1)
 monitors=$(hyprctl monitors | grep -c Monitor)
 if [[ -z "$status" ]]; then
-    hyprctl keyword monitor "eDP-1,2560x1440@165,-1600x0,1.6"
+  hyprctl keyword monitor "eDP-1,2560x1440@165,-1600x0,1.6"
 elif [[ "$monitors" -ge 2 ]]; then
-    hyprctl keyword monitor "eDP-1,disable"
+  hyprctl keyword monitor "eDP-1,disable"
 
-fi 
+fi
